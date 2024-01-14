4681d8b5c0a818fd0425275fd612c0ece706773d
Udhbhav <21889udbhav@gmail.com>
2024-01-14 21:59:12 +0530

    First commit

---
+++ client/ChatSphere/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+      <>
+        <div className="container border-2 flex justify-center items-center ">
+            hello
+        </div>
+      </>
+  )
+}
