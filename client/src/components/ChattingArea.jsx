a74648eab3f484f186d2bfcce4aba8be018e43d0
Dev <flashytesting@gmail.com>
2024-02-06 01:21:46 +0530

    Add client side socket integration and message broadcasting

--- client/src/components/ChattingArea.jsx
+++ client/src/components/ChattingArea.jsx
@@ -1,24 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
+import socket from "../socket";
 
 const ChattingArea = () => {
+    const [isConnected, setIsConnected] = useState(socket.connected);
     const [messages, setMessages] = useState([]);
 
+    useEffect(() => {
+        const onConnect = () => {
+            setIsConnected(true)
+        }
+
+        const onDisconnect = () => {
+            setIsConnected(false)
+        }
+
+        const onReceiveMessage = (message) => {
+            setMessages(prevMessage => {
+                return [...prevMessage, message]
+            })
+        }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        socket.on('receive-message', onReceiveMessage);
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+            socket.off('receive-message', onReceiveMessage);
+        }
+    }, [])
+
     const handleSendMessage = (message) => {
-        {/* TODO: sending of message through socket event */ }
-        setMessages(prevMessages => [...prevMessages, message]);
+        socket.emit('send-message', message);
     }
 
     return (
-        <div className='flex flex-col w-full'>
-            <div className='flex items-center text-white min-h-16 px-5 shadow-lg'>
-                {/* TODO: Here goes the username of person you are talking to*/}
-                Testing User
-            </div>
-            <Messages messages={messages} />
-            <MessageInput onSendMessage={handleSendMessage} />
-        </div>
+        <>
+            {isConnected && (
+                <div className='flex flex-col w-full'>
+                    <div className='flex items-center text-white min-h-16 px-5 shadow-lg'>
+                        {/* TODO: Here goes the username of person you are talking to*/}
+                        Testing User
+                    </div>
+                    <Messages messages={messages} />
+                    <MessageInput onSendMessage={handleSendMessage} />
+                </div>
+            )}
+        </>
     );
 }
 
---
+++ client/src/socket.js
@@ -0,0 +1,7 @@
+import { io } from "socket.io-client";
+
+const SERVER_URL = import.meta.env.SERVER_URL ?? 'http://localhost:3000'
+
+const socket = io(SERVER_URL)
+
+export default socket;
