b3ed9270c7a2154cc4b3a7b9acb8c01d5621c55b
Udhbhav <21889udbhav@gmail.com>
2024-03-04 21:15:18 +0530

    Add create chat clien side

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,11 +1,25 @@
+import axios from 'axios';
 import React from 'react'
 
 export const Users = ({user}) => {
-  return (
-    <>
-        <div className='flex items-center text-white min-h-16 px-5 shadow-sm hover:bg-[#1d2c3b]'>
-            {user.name}
-        </div>
-    </>
-  )
+    console.log(user._id);  
+    const fetchChat = async() => {
+        const {data} = await axios.post('http://localhost:3000/api/chat', {
+            users: user._id
+        }, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem("userInfo")).token}`
+            }
+        });
+
+        console.log(data);
+    }
+
+    return (
+        <>
+            <div className='flex items-center text-white min-h-16 px-5 shadow-sm hover:bg-[#1d2c3b]' onClick={fetchChat}>
+                {user.name}
+            </div>
+        </>
+    )
 }
